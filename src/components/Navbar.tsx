49ee641d995d5fb9dd69e6fd2e258a409a0f2849
calvang <calvin.huang5@gmail.com>
2020-07-12 04:55:29 -0400

    Finalize navbar size configs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,22 +28,22 @@ export default class Navbar extends Component<NavbarProps, NavbarState> {
       {
         link: "/react-github-pages/",
         label: "HOME",
-        icon: "fa fa-home w3-xxlarge"
+        icon: "fa fa-home"
       },
       {
         link: "/react-github-pages/#/About",
         label: "ABOUT",
-        icon: "fa fa-user w3-xxlarge"
+        icon: "fa fa-user"
       },
       {
         link: "/react-github-pages/#/Projects",
         label: "PROJECTS",
-        icon: "fa fa-code w3-xxlarge"
+        icon: "fa fa-code"
       },
       {
         link: "/react-github-pages/#/Contact",
         label: "CONTACT",
-        icon: "fa fa-envelope w3-xxlarge"
+        icon: "fa fa-envelope"
       }
     ]
     return (
@@ -62,7 +62,7 @@ export default class Navbar extends Component<NavbarProps, NavbarState> {
                   links.map((page, i) => {
                     return (
                       <a href={page.link} className="w3-bar-item w3-button w3-padding-small w3-hover-black">
-                        <i className={page.icon}></i>
+                        <i className={page.icon + " w3-xxlarge"}></i>
                         <p>{page.label}</p>
                       </a>
                     )
@@ -71,25 +71,26 @@ export default class Navbar extends Component<NavbarProps, NavbarState> {
               </div>
             </nav> 
             {/*Navbar on medium screens*/}
-            <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-hide-large w3-center"
-              style={{ width: "auto", background: "#222" }}>
-              <button className="w3-bar-item w3-button w3-hover-black" 
+            <div className="w3-top w3-small w3-hide-small w3-hide-large w3-left">
+              <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-medium">
+                <button className="w3-bar-item w3-button w3-padding"
+                  style={{ width: "10%" }}
                   onClick={this.toggleMenu}>
-                  <img src={logo} style={{ width: "100%", zoom: "10%" }} alt="logo" />
-              </button>
-              <div>
+                  <img src={logo} style={{ left: "5%", height: "auto", width: "50%", zoom: "100%" }} alt="logo" />
+                </button>
                 {
                   links.map((page, i) => {
                     return (
-                      <a href={page.link} className="w3-bar-item w3-button w3-padding-small w3-hover-black">
-                        <i className={page.icon}></i>
-                        <p>{page.label}</p>
+                      <a href={page.link} className="w3-bar-item w3-button"
+                        style={{ fontSize: "calc(1px + 2vmin)", verticalAlign: "center", width:"22.5%" }}>
+                        <i className={page.icon + " w3-large"} ></i>
+                        {" " + page.label}
                       </a>
                     )
                   })
                 }
               </div>
-            </nav> 
+            </div> 
             {/*Navbar on small screens*/}
             <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
               <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-medium">
@@ -100,7 +101,7 @@ export default class Navbar extends Component<NavbarProps, NavbarState> {
                 {
                   links.map((page, i) => {
                     return (
-                      <a href={page.link} className="w3-bar-item w3-button" style={{width:"25% !important"}}>
+                      <a href={page.link} className="w3-bar-item w3-button" style={{width:"20%"}}>
                         {page.label}
                       </a>
                     )
